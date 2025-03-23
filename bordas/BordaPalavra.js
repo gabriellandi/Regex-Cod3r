4bd8b82a4b1be3b8c69fc2ce99aca2c26108c582
gabriellandi <gabriel.landi@gmail.com>
2025-03-23 15:57:29 -0300

    receitas

--- bordas/BordaPalavra.js
+++ bordas/BordaPalavra.js
@@ -1,3 +1,12 @@
 const texto1 = 'dia diatonico diafragma media wikipedia bom_dia melodia radial'
 
-console.log(texto1.match())
+console.log(texto1.match(/\bdia\w+/gi))
+console.log(texto1.match(/\w+dia\b/gi))
+console.log(texto1.match(/\w+dia\w+/gi))
+console.log(texto1.match(/\bdia\b/gi))
+
+
+// borda é não \w, que é [^A-Za-z0-9_]... temos problemas com acentos, pois são bordas, e a lingua portuguesa é recheada deles
+const texto2 = 'dia diatônico diafragma, média wikipédia bom-dia melodia radial'
+console.log(texto2.match(/\bdia\b/gi))
+console.log(texto2.match(/(\S*)?dia(\S*)?/gi)) // a vírgula entra!
